72db9c51e425c9b3ef86d28300b209bbca81d9ca
Tkachmaxim <tkachmaxim23@gmail.com>
2021-09-06 15:26:45 +0300

    yet

--- hello_world/app.py
+++ hello_world/app.py
@@ -40,7 +40,5 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "body": json.dump(
-            result.__next__()
-    ),
+        "body": json.dump(result.__next__())
     }
